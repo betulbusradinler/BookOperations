80c09c1553d218ffa3d9cd81b0afab47202f4547
Büşra Betül Dinler <busrabetuldinler@192.168.1.36>
2025-05-09 22:33:29 +0300

    Create Exception Handle Middleware

---
+++ Middleware/CustomExceptionMiddleware.cs
@@ -0,0 +1,55 @@
+using System.Diagnostics;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace BookOperations.Middleware;
+
+public class CustomExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    public CustomExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    { 
+        var watch = Stopwatch.StartNew();
+        try
+        {
+            string message = "[Request] HTPP " + context.Request.Method + "-" + context.Request.Path;
+            Console.WriteLine(message);
+            await _next(context);
+            watch.Stop();
+            message = "[Response] HTPP " + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + "ms";
+            Console.WriteLine(message);
+        }
+        catch(Exception ex)
+        {
+            watch.Stop();
+            await HandleException(context,ex,watch);
+        }
+    }
+
+    private Task HandleException(HttpContext context, Exception ex, Stopwatch watch)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+        string message = " [Error] HTTP  " + context.Request.Method + "-" + context.Response.StatusCode + " ErrorMessage " + ex.Message + " in " + watch.Elapsed.TotalMilliseconds + "ms";
+        Console.WriteLine(message);
+
+        // HATAYI ÇÖZ VE TEST  ET
+
+
+        var result = JsonConvert.SerializeObject(new {error = ex.Message}, Formatting.None);
+
+       return context.Response.WriteAsync(result);
+    }
+}
+
+public static class CustomExceptionMiddlewareExtension
+{
+    public static IApplicationBuilder UseCustomExceptionMiddleware(this IApplicationBuilder applicationBuilder){
+        return applicationBuilder.UseMiddleware<CustomExceptionMiddleware>();
+    }
+}
